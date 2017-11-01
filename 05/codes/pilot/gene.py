25bebe95d097a88020a754e3d1f237f0854d3392
atulsinha007 <sinhaa297@gmail.com>
2017-11-02 00:43:43 +0530

    gene modified

--- 05/codes/pilot/gene.py
+++ 05/codes/pilot/gene.py
@@ -7,17 +7,16 @@ class Node:
 
 
 class Conn:
-    def  __init__(self,innov_num,source,destination,weight):
+    def  __init__(self,innov_num,couple,weight,status = True):
         self.status=status          #determines enabled or disabled
-        self.source=source          #a node object
-        self.destination=destination#same as above
+        self.source=couple[0]          #a node object
+        self.destination=couple[1]#same as above
         self.weight=weight
         self.innov_num=innov_num
     def get_status(self):
         return self.status
     def get_innov_num(self):
         return self.innov_num
-
     def get_couple(self):
         return (self.source,self.destination)
 
@@ -26,5 +25,6 @@ class BiasConn(Conn):
     def __init__(self,out_node,weight):
         numb=-1
         nature=-1
-        Conn.__init__(-1,Node(numb,nature),out_node,weight)
+        status = True
+        Conn.__init__(-1,(Node(numb,nature),out_node),weight,status)
 
