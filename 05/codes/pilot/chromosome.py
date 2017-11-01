ca6c0ad5b78cc96bd97245f6f140954ac120ac1a
atulsinha007 <sinhaa297@gmail.com>
2017-11-02 01:47:58 +0530

    Indentation changed again

--- 05/codes/pilot/chromosome.py
+++ 05/codes/pilot/chromosome.py
@@ -7,4 +7,4 @@ class Chromosome:
         self.node_arr = []	#list of node objects
         self.conn_arr = []	#list of conn objects
         self.bias_arr = []	#list of BiasNode objects
-        self.dob = dob 		# the generation in which it was created.
+        self.dob = dob 		#the generation in which it was created.
