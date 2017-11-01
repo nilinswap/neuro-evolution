6c1b107db1c6c70b83dbab602b2bc22049f6012c
Swapnil Sharma <swapnilsharma@Swapnils-MacBook-Air.local>
2017-11-02 00:15:54 +0530

    neat_encoding being constructed

---
+++ 05/codes/pilot/chromosome.py
@@ -0,0 +1,10 @@
+#chromosome.py
+
+import gene
+
+class Chromosome:
+    def __init__(self,dob):
+        self.node_arr=[]#list of node objects
+        self.conn_arr=[]#list of conn objects
+        self.bias_arr=[]#list of BiasNode objects
+        self.dob=dob # the generation in which it was created.
