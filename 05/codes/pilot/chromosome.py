4b7c19dc97ddd5385b79d487fa49c5827d20fd20
atulsinha007 <sinhaa297@gmail.com>
2017-11-02 21:35:00 +0530

    Merged with feedforwardcm tested

--- 05/codes/pilot/chromosome.py
+++ 05/codes/pilot/chromosome.py
@@ -3,31 +3,26 @@
 import gene
 node_ctr = None
 innov_ctr = None
+
 class Chromosome:
-<<<<<<< HEAD
-    def __init__(self,dob):
-        self.node_arr = []	#list of node objects
-        self.conn_arr = []	#list of conn objects
-        self.bias_arr = []	#list of BiasNode objects
-        self.dob = dob 		#the generation in which it was created.
-=======
+
     def __init__(self,dob,node_arr=[],conn_arr=[],bias_arr=[]):
-        self.node_arr=node_arr#list of node objects
-        self.conn_arr=conn_arr#list of conn objects
-        self.bias_arr=bias_arr#list of BiasNode objects
-        self.dob=dob # the generation in which it was created.
+        self.node_arr = node_arr	#list of node objects
+        self.conn_arr = conn_arr	#list of conn objects
+        self.bias_arr = bias_arr	#list of BiasNode objects
+        self.dob = dob 				#the generation in which it was created.
+
     def rand_init(self,inputdim,outputdim,rng):
         global node_ctr
         global innov_ctr
-        node_ctr=inputdim+outputdim+1
+        node_ctr = inputdim+outputdim+1
         innov_ctr = 0
         lisI = [gene.Node(num_setter,'I') for num_setter in range(1,node_ctr-outputdim)]
         lisO =  [gene.Node(num_setter,'O') for num_setter in range(inputdim+1,node_ctr)]
         self.node_arr = lisI + lisO
         for inputt in lisI:
             for outputt in lisO:
-                innov_ctr+=1
+                innov_ctr += 1
                 self.conn_arr.append( gene.Conn(innov_ctr, inputt, outputt, rng.random(), status=True))
-        self.bias_arr=[gene.BiasConn(outputt,rng.random()) for outputt in lisO]
-        self.dob=0
->>>>>>> 98264075887727120ec2815e454d62d7a04c24e8
+        self.bias_arr = [gene.BiasConn(outputt,rng.random()) for outputt in lisO]
+        self.dob = 0
--- 05/codes/pilot/gene.py
+++ 05/codes/pilot/gene.py
@@ -3,47 +3,26 @@
 class Node:
 	def __init__(self, node_num, nature):
 		self.node_num = node_num
-		self.nature = nature	#'I','H1','H2','O'
+		self.nature = nature			#'I','H1','H2','O'
 
 class Conn:
-<<<<<<< HEAD
-	def __init__(self, innov_num, couple, weight, status = True):
-		self.innov_num = innov_num		#determines enabled or disabled
-		self.source = couple[0]			#a node object
-		self.destination = couple[1]	#same as above
-		self,weight = weight
-		self.status = status
-=======
     def  __init__(self,innov_num,couple,weight,status = True):
-        self.status=status          #determines enabled or disabled
-        self.source=couple[0]          #a node object
-        self.destination=couple[1]#same as above
-        self.weight=weight
-        self.innov_num=innov_num
+        self.status = status          	#determines enabled or disabled
+        self.source = couple[0]       	#a node object
+        self.destination = couple[1]	#same as above
+        self.weight = weight
+        self.innov_num = innov_num
 
     def get_couple(self):
-        return (self.source,self.destination)
->>>>>>> 98264075887727120ec2815e454d62d7a04c24e8
-
-	def get_status(self):
-		return self.status
-
-<<<<<<< HEAD
-	def get_innov_num(self):
-		return self.innov_num
-
-	def get_weight(self):
-		return self.weight
-=======
+        return (self.source, self.destination)
 
 
 class BiasConn(Conn):
-    def __init__(self,out_node,weight):
+    def __init__(self, out_node, weight):
         numb=-1
         nature=-1
         status = True
-        Conn.__init__(-1,(Node(numb,nature),out_node),weight,status)
->>>>>>> 98264075887727120ec2815e454d62d7a04c24e8
+        Conn.__init__(-1, (Node(numb,nature), out_node), weight, status)
 
 	def get_couple(self):
 		return (self.source, self.destination)
