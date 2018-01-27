f715f92f8fb30a7272c843fc026197ca51719fdc
nilinswap <nilinswap@gmail.com>
2018-01-28 03:01:19 +0530

    run_just_src.sh added

--- postmidsem/codes/main_folder/pilot/auto_log_generator1.py
+++
@@ -1,5 +0,0 @@
-#sys.test
-import os
-import sys
-for i in range(int(sys.argv[1])):
-    os.system("python3 main1.py")
--- postmidsem/codes/main_folder/pilot/auto_log_generator_approach1.py
+++
@@ -1,4 +0,0 @@
-import os
-import sys
-for i in range(int(sys.argv[1])):
-    os.system("python3 main1_approach1.py")
