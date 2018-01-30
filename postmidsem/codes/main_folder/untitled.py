53afc4257b6b627a9f424a22b44d7e8685fdc15d
nilinswap <nilinswap@gmail.com>
2018-01-31 01:53:30 +0530

    resize and pad will be done before hog

--- postmidsem/codes/main_folder/untitled.py
+++ postmidsem/codes/main_folder/untitled.py
@@ -27,19 +27,18 @@ desired_size = 250000
 
 
 desired_size = 500
-file_st= '/home/placements2018/forgit/Dataset3/062.eiffel-tower/062_0064.jpg'
+file_st= '/home/placements2018/forgit/Dataset3/060.duck/060_0032.jpg'
 image = PIL.Image.open(file_st)
-old_size = image.size[0]  # old_size[0] is in (width, height) format
+old_size = image.size  # old_size[0] is in (width, height) format
 ratio = float(desired_size)/max(old_size)
 new_size = tuple([int(x*ratio) for x in old_size])
-image = image.resize(new_size, Image.ANTIALIAS)
-image = image.resize(new_size, Image.ANTIALIAS)
+image = image.resize(new_size, PIL.Image.ANTIALIAS)
+image = image.resize(new_size, PIL.Image.ANTIALIAS)
 # create a new image and paste the resized on it
-new_im = Image.new("L", (desired_size, desired_size))
+new_im = PIL.Image.new("RGB", (desired_size, desired_size))
 new_im.paste(image, ((desired_size-new_size[0])//2,
                     (desired_size-new_size[1])//2))
-image = new_image
-
+image = new_im
 image = np.asarray(image)
 image = to_gray(image)
 print(image.shape)
